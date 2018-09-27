3723aa7d968b2b0964abc6896148ec768fed4e47
Patrick Teivas <teivas.2011@mail.ru>
2018-09-27 09:20:30 +0300

    Commit

---
+++ WeatherApp.Core/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace WeatherApp
+{
+    class DataService
+    {
+        public static async Task<dynamic> GetDataFromService(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            dynamic data = null;
+
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+
+            return data;
+        }
+    }
+}
