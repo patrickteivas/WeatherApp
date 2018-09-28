5d7ba610941cb8f5295bae152fcb97fa2de880ae
Patrick Teivas <teivas.2011@mail.ru>
2018-09-28 14:50:26 +0300

    Commit

--- WeatherApp.Core/DataService.cs
+++ WeatherApp.Core/DataService.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Newtonsoft.Json;
 
 namespace WeatherApp
